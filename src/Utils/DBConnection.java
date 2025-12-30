18eab0cdce0a8b7683646dc0fa0b4b2e124e3e1a
adgator101 <aadityathapa977@gmail.com>
2025-12-30 08:28:31 +0545

    refactor: update DBconnection to use Constructor based singleton pattern

--- src/Utils/DBConnection.java
+++ src/Utils/DBConnection.java
@@ -1,4 +1,4 @@
-//package Workshop;
+package Utils;//package Workshop;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,21 +6,16 @@ import java.sql.SQLException;
 
 public class DBConnection {
     static final String DB_URL = "jdbc:mysql://localhost/";
-    static final String DB_USER = "<your_database_user>";
-    static final String DB_PASSWORD = "<your_database_password>";
-    private static DBConnection instance;
+    static final String DB_USER = "aaditya";
+    static final String DB_PASSWORD = "admin";
+    public static DBConnection instance;
 
     private DBConnection() {
-    }
-
-    public static DBConnection getInstance() {
         if (instance == null) {
             instance = new DBConnection();
         }
-        return instance;
     }
-
-    static Connection getDbConnection() throws SQLException {
+    public static Connection getDbConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 }
